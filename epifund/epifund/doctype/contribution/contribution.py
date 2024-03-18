d81060b88e36e093dfbb8892a15126f4b7d5b335
George Everitt <geveritt@appliedrelevance.com>
2024-03-18 02:02:20 +0000

    Updated Contribution Report

--- epifund/epifund/doctype/contribution/contribution.py
+++ epifund/epifund/doctype/contribution/contribution.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2024, Applied Relevance and contributors
 # For license information, please see license.txt
 
-# import frappe
 from frappe.model.document import Document
 
-
 class Contribution(Document):
-	pass
+    def before_save(self):
+        if self.unit == "Hours":
+            self.points = self.value * self.fhrr * self.point_conversion_rate
+        elif self.unit == "Cash":
+            self.points = self.value * self.point_conversion_rate
+        elif self.unit == "Resources":
+            # Assuming resources are valued similarly to cash contributions
+            self.points = self.value * self.point_conversion_rate
+        else:
+            self.points = 0
